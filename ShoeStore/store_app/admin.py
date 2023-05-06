5c43d4c4ffa23fc9910c1a13d4c91f42b28153fb
DNAnh01 <donguyenanhgithub@gmail.com>
2023-05-06 22:48:37 +0700

    frontend and create models

---
+++ ShoeStore/store_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register(Categories)
+admin.site.register(Brand)
+admin.site.register(Color)
+admin.site.register(Filter_Price)
