73315a6367f10ba6f45ef38c35a4da279b905c29
DNAnh01 <donguyenanhgithub@gmail.com>
2023-05-15 08:21:41 +0700

    create contact model

--- ShoeStore/store_app/models.py
+++ ShoeStore/store_app/models.py
@@ -74,3 +74,13 @@ class Images(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Contact_us(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    subject = models.CharField(max_length=300)
+    message = models.TextField
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
